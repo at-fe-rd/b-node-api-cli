19bce616e7d3de505475704e0e5c1ff88bc05e0a
at-vinguyen <vi.nguyen@asiantech.vn>
2019-01-30 12:05:43 +0700

    write sample code in template

--- bin/templates/crud/controller.js
+++ bin/templates/crud/controller.js
@@ -4,6 +4,17 @@ let { actions } = require('@core/based/controller')({
 
 /**
  * Adding new action here
+ * For example:
+ * actions.filterByName = (req, reply) => {
+ *  // todo
+ * }
+ * or
+ * const newActions = {
+ *  filterByName = (req, reply) => {
+ *    // todo
+ *  }
+ * }
+ * { ...actions, ...newActions }
  */
 
 exports.__MODEL__Action = actions
--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -4,6 +4,10 @@ let { actions } = require('@core/based/controller')({
 
 /**
  * Adding new action here
+ * For example:
+ * actions.filterByName = (req, reply) => {
+ *    // todo
+ * }
  */
 
 exports.bookAction = actions
